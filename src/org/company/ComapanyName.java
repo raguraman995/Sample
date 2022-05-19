e770fb7a5335884d1e41bb42232af59466d4eb5c
raguraman995 <raguraman995@gmail.com>
2022-05-19 13:35:58 +0530

    new code

---
+++ src/org/company/ComapanyName.java
@@ -0,0 +1,19 @@
+package org.company;
+
+import org.client.Client;
+
+public class ComapanyName extends Client {
+	public void comapanyName() {
+		System.out.println("print comapanyName");
+	}
+	public static void main(String[] args) {
+		ComapanyName c = new ComapanyName();
+		c.comapanyName();
+		c.clientName();
+		
+	}
+	
+	
+	}
+
+
